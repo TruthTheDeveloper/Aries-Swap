59b51abb6f7aae3426ebcc11dd603b308df87130
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-06-27 16:44:02 +0100

    make a push to the development branch

--- containers/landingPage/LandingPage.tsx
+++ containers/landingPage/LandingPage.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 
 const LandingPage: NextPage = () => {
     return (
-        <h1>Hello world guys</h1>
+        <h1>Hello world guys lets get to work</h1>
     )
   }
   
